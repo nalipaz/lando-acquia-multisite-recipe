23f657e614a622f06611b7056219d695ece88c22
Nicholas Alipaz <nicholas@alipaz.net>
2018-03-20 08:59:43 -0700

    Update sql-sync-all.sh

--- lando/tooling/sql-sync-all.sh
+++ lando/tooling/sql-sync-all.sh
@@ -2,9 +2,9 @@
 
 drush cc drush --quiet
 # Ascertain our appname from one of the prod aliases.
-appname=`lando drush sa|grep -Po '@[a-z]+.prod\r'|sed -E 's/@([a-z]+).prod/\1/'`
+appname=`drush sa|grep -Po '@[a-z]+.prod\r'|sed -E 's/@([a-z]+).prod/\1/'`
 
-for site in $(lando drush sa|grep -Po '@[a-z]+\r'|grep -v '@none'); do
+for site in $(drush sa|grep -Po '@[a-z]+\r'|grep -v '@none'); do
   echo "[${site}] Synchronizing SQL from ${1} to lando..."
   drush sql-sync @${appname}.${1}.${site} @${site} -y
 done
