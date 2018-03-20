b1cb3253e47a2772ab4389e7a761502884824e9f
Nicholas Alipaz <nicholas@alipaz.net>
2018-03-20 07:47:56 -0700

    Update settings.php

--- docroot/sites/site1.com/settings.php
+++ docroot/sites/site1.com/settings.php
@@ -4,7 +4,6 @@
 
 // ... followed by ...
 
-// The following variable is Acquia-centric but could easily be swapped out for some other environment variable.
 // Include environment specific settings files if they exist.
 if (key_exists('AH_SITE_ENVIRONMENT', $_ENV)) {
   $environment_settings = $site_directory . '/settings.' . $_ENV['AH_SITE_ENVIRONMENT'] . '.php';
